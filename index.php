aab7eb5854ec9b2de0c7bea21df49053f36c4d68
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 11:17:01 +0100

    Modify php index

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-
+echo 'Works  !'
 $ip       = $_ENV['database_ip'];
 $username = $_ENV['database_user'];
 $password = $_ENV['database_password'];
+
+echo "ip: $ip username: $username password: $password"
 
 $dbconn = pg_connect("host=$ip port=5432 dbname=localdb user=$username password=$password");
 $result = pg_query($conn, "select * from pg_stat_activity");
