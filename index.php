26c6c70519cb7f0280fa07ebfd1c1871d3e0af72
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 09:52:35 +0100

    Add php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$ip       = $_ENV['database_ip'];
+$username = $_ENV['database_user'];
+$password = $_ENV['database_password'];
+
+$dbconn = pg_connect("host=$ip port=5432 dbname=localdb user=$username password=$password");
+$result = pg_query($conn, "select * from pg_stat_activity");
+while ($row = pg_fetch_row($result)) {
+    echo "id: $row[0]  name: $row[1] descript: $row[2] price: $row[3]";
+  }
