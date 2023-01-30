8d59d4d446abde274c744472a48d7ccc8b347912
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 20:59:28 +0100

    db credential

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 $database_host      = $_ENV['database-ip'];
 $database_username  = $_ENV['database-username'];
-$database_username  = $_ENV['database-password'];
-
+$database_password  = $_ENV['database-password'];
 
 try {
-  $pdo = new PDO('pgsql:host=thibaultpostgre.postgres.database.azure.com;port=5432;dbname=localdb', 'thibault@thibaultpostgre', 'jira06Ra');
+  $pdo = new PDO("pgsql:host=$database_host;port=5432;dbname=localdb", $database_username, $database_password);
 } catch (Exception $e) {
   echo 'Exception reçue : ', $e->getMessage(), "\n";
 }
@@ -15,6 +14,8 @@ $stmt = $pdo->query("SELECT * FROM products");
 while ($row = $stmt->fetch()) {
   $response[] = array("Id" => $row[0], "Name" => $row[1], "Desc" => $row[2], "Prix" => $row[3]);
 }
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 header('Content-Type: application/json');
 echo json_encode($response, JSON_PRETTY_PRINT);
 $pdo->$connection = null;
