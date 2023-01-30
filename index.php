a1f6bf8e51db1f4c45524125686ab86aba6aa4f8
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 21:07:03 +0100

    cors policy

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $database_host      = $_ENV['database-ip'];
 $database_username  = $_ENV['database-username'];
 $database_password  = $_ENV['database-password'];
 
-echo $_ENV;
+echo "host:$host username:$username password:$password";
+echo "<br>";
 
 try {
   $pdo = new PDO("pgsql:host=$database_host;port=5432;dbname=localdb", $database_username, $database_password);
@@ -16,9 +17,9 @@ $stmt = $pdo->query("SELECT * FROM products");
 while ($row = $stmt->fetch()) {
   $response[] = array("Id" => $row[0], "Name" => $row[1], "Desc" => $row[2], "Prix" => $row[3]);
 }
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Content-Type: application/json");
-echo json_encode($response);
+header("Access-Control-Allow-Origin: ");
+header("Access-Control-Allow-Headers:");
+header('Content-Type: application/json');
+echo json_encode($response, JSON_PRETTY_PRINT);
 $pdo->$connection = null;
 ?>
