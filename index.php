52707a319c8040bf9ab765cbe920ed2c3eb97627
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 11:50:31 +0100

    modify

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip       = $_ENV['database_ip'];
 $username = $_ENV['database_user'];
 $password = $_ENV['database_password'];
 
-$dbconn = pg_connect("host=$ip port=5432 dbname=localdb user=$username password=$password");
+$conn = pg_connect("host=$ip port=5432 dbname=localdb user=$username password=$password");
 $result = pg_query($conn, "select * from products");
 while ($row = pg_fetch_row($result)) {
     echo "id: $row[0]  name: $row[1] descript: $row[2] price: $row[3]";
