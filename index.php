22cd5a3adbe33ccb403089f4163f4597e752c453
thibault.boulay <thibault.boulay@nolonely.fr>
2023-01-30 12:14:34 +0100

    db

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ $ip       = $_ENV['database_ip'];
 $username = $_ENV['database_user'];
 $password = $_ENV['database_password'];
 
-$conn = pg_connect("host=https://app-service-thibault-boulay.azurewebsites.net dbname=localdb user=thibault password=jira06Ra");
+$conn = pg_connect("host=thibaultpostgre.postgres.database.azure.com dbname=localdb user=thibault password=jira06Ra");
 $result = pg_query($conn, "select * from products");
 while ($row = pg_fetch_row($result)) {
     echo "id: $row[0]  name: $row[1] descript: $row[2] price: $row[3]";
 }
+?>
